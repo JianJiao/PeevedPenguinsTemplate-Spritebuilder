7b12e8eab83ee2ed7bebf65e8b2c9e136627e187
JianJiao <jianj@andrew.cmu.edu>
2015-01-30 14:42:56 -0800

    Automatically Committed for MakeGamesWithUs

--- PeevedPenguins.spritebuilder/Source/Gameplay.m
+++ PeevedPenguins.spritebuilder/Source/Gameplay.m
@@ -41,9 +41,9 @@
     CGPoint force = ccpMult(launchDirection, 8000);
     [penguin.physicsBody applyForce:force];
     // ensure followed object is in visible are when starting
-    self.position = ccp(50, 50);
-//    CCActionFollow *follow = [CCActionFollow actionWithTarget:penguin worldBoundary:self.boundingBox];
-//    [self runAction:follow];
+    self.position = ccp(0, 0);
+    CCActionFollow *follow = [CCActionFollow actionWithTarget:penguin worldBoundary:self.boundingBox];
+    [self runAction:follow];
 }
 
 - (void)retry {
