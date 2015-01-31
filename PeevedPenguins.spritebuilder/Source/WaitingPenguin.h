2b9ba364cc8f6ab5bcfb75575067042076425c4f
JianJiao <jianj@andrew.cmu.edu>
2015-01-30 20:56:29 -0800

    after step7, week3

---
+++ PeevedPenguins.spritebuilder/Source/WaitingPenguin.h
@@ -0,0 +1,13 @@
+//
+//  WaitingPenguin.h
+//  PeevedPenguins
+//
+//  Created by Jian Jiao on 1/30/15.
+//  Copyright (c) 2015 Apportable. All rights reserved.
+//
+
+#import "CCSprite.h"
+
+@interface WaitingPenguin : CCSprite
+
+@end
